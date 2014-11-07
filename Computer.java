dbb7d27d461d633f66b9584b4ecec7d37e1bde97
eryueliang <liangey0422@gmail.com>
2014-11-08 00:00:43 +0800

    Create Computer.java

---
+++ Computer.java
@@ -0,0 +1,18 @@
+
+
+/**
+ * Created by liangey on 14-11-7.
+ */
+public class Computer {
+    public String screen;
+    public String cpu;
+
+    public void setScreen(String screen) {
+        this.screen = screen;
+    }
+
+    public void setCpu(String cpu)
+    {
+        this.cpu = cpu;
+    }
+}
