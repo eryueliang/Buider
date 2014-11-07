aa60b108058b62046ed62877f7340e8e79160f04
eryueliang <liangey0422@gmail.com>
2014-11-07 23:59:07 +0800

    Create Medium.java

---
+++ Medium.java
@@ -0,0 +1,16 @@
+import com.Computer;
+
+/**
+ * Created by liangey on 14-11-7.
+ */
+public class Medium extends Computer{
+    @Override
+    public void setCpu(String cpu) {
+        super.setCpu("Medium");
+    }
+
+    @Override
+    public void setScreen(String screen) {
+        super.setScreen("Medium");
+    }
+}
